e0df8d71ec26fba743ddb6c869406eba2bd38a32
aemerson1 <alexis.g.emerson@gmail.com>
2025-09-13 12:04:46 -0400

    input files to dictionary

--- cli/main.py
+++ cli/main.py
@@ -1,5 +1,6 @@
 import click
 import csv
+import os
 
 @click.command()
 @click.argument('input_file', type=click.Path(exists=True, file_okay=True, dir_okay=False, readable=True))
@@ -7,12 +8,23 @@ import csv
 
 def main(input_file, mapping_file):
     """Reads a CSV file from the given path and prints its content."""
-    with open(file_path, mode='r', newline='', encoding='utf-8') as csv_file:
-        reader = csv.reader(csv_file)
-        for row in reader:
-            print(row)
+    """If field is empty don't include it."""
     
-    print(csv_to_key_value_dict(mapping_file))
+    with open(input_file, mode='r', newline='', encoding='utf-8') as csv_file:
+        reader = csv.DictReader(csv_file)
+        
+        filename = os.path.splitext(input_file)[0]
+
+        result = []
+        for row in reader:
+            result.append({filename: dict(row)})
+
+    print(result[0])
+        
+    # print(csv_to_key_value_dict(mapping_file))
+
+def create_object(column_names, row, file_name):
+    pass
 
 def csv_to_key_value_dict(file_path):
     """Convert 2-column CSV to key-value dictionary"""
